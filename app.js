1a350774ab72f3e61c50301ffa93658349dce3a5
Francisco Trabucco <ftrabucco@vates.org>
2020-09-29 13:03:42 -0300

    cap-1-video-17- .Filter , .find y métodos para arreglos

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-// Spread operator
+// Métodos en arreglos
 
-let lenguajes = ['JavaScript', 'PHP','Python'];
-let frameworks= ['ReactJS','Laravel','Django'];
+const personas =[
+    {nombre: "Juan", edad:20, aprendiendo:"JavaScript"},
+    {nombre: "Pablo", edad:32, aprendiendo:"Python"},
+    {nombre: "Alejandra", edad:18, aprendiendo:"PHP"},
+    {nombre: "Luis", edad:40, aprendiendo:"Java"},
+    {nombre: "Roberto", edad:19, aprendiendo:"Java"}
+]
 
-lt [ultimo]=[...lenguajes].reverse();
+console.log(personas);
 
-console.log(ultimo)
+//mayores de 28 años
 
+const mayores = personas.filter(persona=>{
+    return persona.edad>28;
+});
+
+console.log(mayores);
+
+// que aprende alejandra y su edad
+const alejandra = personas.find(persona=>{
+    return persona.nombre === 'Alejandra';
+});
+console.log("Alejandra esta aprendiendo: " + alejandra.aprendiendo);
+
+let total = personas.reduce((edadTotal, persona) =>{
+    return edadTotal + persona.edad;
+},0);
+
+console.log("promedio de edad: " + total/personas.length)
+console.log(total)
