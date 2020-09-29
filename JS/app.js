42db5dc0e1402d7508a5701dbbcd68ca4073117b
Francisco Trabucco <ftrabucco@vates.org>
2020-09-29 18:03:38 -0300

    cap-1-video-23-Modulos en ES6

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+import {nombreTarea} from "./tareas.mjs"
+
+console.log({nombreTarea});
+
+
+/*mport Tarea from "./tareas.mjs"
+
+
+const tarea1=new Tarea('Aprender JS','Urg')
+
+console.log(tarea1.mostrar());
+
+*/
