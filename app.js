b6c915c03ab9938c7feb3339edc2a9bd262af2a0
Francisco Trabucco <ftrabucco@vates.org>
2020-09-29 11:47:15 -0300

    cap-1-video-11- Prototype

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 // Object literal
 const persona={
     nombre:"Juan",
@@ -7,7 +6,11 @@ const persona={
     edad:34
 }
 
-console.log(persona)
+//console.log(persona)
+/*
+const mostrarCliente=mostrarInformacionTarea(persona.nombre,
+    persona.profesion)
+console.log(mostrarCliente)*/
 
 // Object Construtctor
 function Tarea(nombre, urgencia){
@@ -15,17 +18,21 @@ function Tarea(nombre, urgencia){
     this.urgencia = urgencia;
 }
 
+//Agregar un prototype a tarea:
+Tarea.prototype.mostrarInformacionTarea = function (){
+    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
+}
+
+
+// Crear nuevas tareas:
 const tarea1 = new Tarea("Aprender JavaScript y React", "Urgente");
-const tarea2 = new Tarea("Preparar café", "Urgente");
-const tarea3 = new Tarea("Pasear al perro", "Media");
-const tarea4 = new Tarea("Conocer a mis suegros", "Baja");
+const tarea2 = new Tarea("Pasear al perro", "Media");
+console.log(tarea1);
+console.log(tarea2);
+console.log(tarea1.mostrarInformacionTarea());
+console.log(tarea2.mostrarInformacionTarea());
 
 
-
-console.log(tarea1)
-console.log(tarea2)
-console.log(tarea3)
-console.log(tarea4)
 
 
 
