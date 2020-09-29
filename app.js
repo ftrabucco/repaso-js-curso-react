51e343fe660b9b1d8e29a1f2b1fd0419b28dc71c
Francisco Trabucco <ftrabucco@vates.org>
2020-09-29 11:19:30 -0300

    cap-1-video-10- Object constructor

--- app.js
+++ app.js
@@ -7,15 +7,25 @@ const persona={
     edad:34
 }
 
-const persona2={
-    nombre:"Juan",
-    profesion:"Desarrollador Web",
-    edad:34
+console.log(persona)
+
+// Object Construtctor
+function Tarea(nombre, urgencia){
+    this.nombre = nombre;
+    this.urgencia = urgencia;
 }
 
-console.log(persona)
-console.log(persona2)
+const tarea1 = new Tarea("Aprender JavaScript y React", "Urgente");
+const tarea2 = new Tarea("Preparar café", "Urgente");
+const tarea3 = new Tarea("Pasear al perro", "Media");
+const tarea4 = new Tarea("Conocer a mis suegros", "Baja");
 
+
+
+console.log(tarea1)
+console.log(tarea2)
+console.log(tarea3)
+console.log(tarea4)
 
 
 
