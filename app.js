0df9797de8ff5b938637030fb29ebc6492e50e82
Francisco Trabucco <ftrabucco@vates.org>
2020-09-29 11:11:54 -0300

    cap-1-video-9- Object literal

---
+++ app.js
@@ -0,0 +1,29 @@
+
+
+// Object literal
+const persona={
+    nombre:"Juan",
+    profesion:"Desarrollador Web",
+    edad:34
+}
+
+const persona2={
+    nombre:"Juan",
+    profesion:"Desarrollador Web",
+    edad:34
+}
+
+console.log(persona)
+console.log(persona2)
+
+
+
+
+
+
+
+
+
+
+
+
